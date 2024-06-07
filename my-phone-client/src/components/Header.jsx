c6ce21384f19f7623b9a22cf30ca2d2d3cda2311
arifurrahmansajid <iamnobody3456@gmail.com>
2024-06-07 16:38:49 +0600

    first commit

---
+++ my-phone-client/src/components/Header.jsx
@@ -0,0 +1,14 @@
+
+
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div>
+            
+            <Link to="/phones">Phones</Link>
+        </div>
+    );
+};
+
+export default Header;
